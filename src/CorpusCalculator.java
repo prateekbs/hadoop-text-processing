7db4f1509f35a0b43e5d5fabce42a2aeffa6fae7
Prateek Swamy <bs.prateek@gmail.com>
2015-05-10 17:26:05 -0700

    Text file to position,word output

--- src/CorpusCalculator.java
+++ src/CorpusCalculator.java
@@ -12,29 +12,33 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
         
 public class CorpusCalculator {
         
- public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
+ public static class Map extends Mapper<LongWritable, Text, Text, Text> {
     private Text word = new Text();
-        
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         StringTokenizer tokenizer = new StringTokenizer(line);
+        int position=0;
         while (tokenizer.hasMoreTokens()) {
+        	position=position+1;
             word.set(tokenizer.nextToken());
-            context.write(word, one);
+            context.write(new Text(Integer.toString(position)),word);
         }
     }
  } 
         
- public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+ public static class Reduce extends Reducer<Text, Text, Text, Text> {
+	private Text word = new Text();
+    public void reduce(Text key, Iterator<Text> values, Context context) 
       throws IOException, InterruptedException {
-        int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
-        }
-        context.write(key, new IntWritable(sum));
+      StringBuilder toReturn = new StringBuilder();
+      boolean first = true;
+      while (values.hasNext()){
+          if (!first)
+            toReturn.append(", ");
+          first=false;
+          toReturn.append(values.next().toString());
+      }
+      context.write(key, new Text(toReturn.toString()));
     }
  }
         
@@ -44,7 +48,7 @@ public class CorpusCalculator {
         Job job = new Job(conf, "wordcount");
     
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
         
     job.setMapperClass(Map.class);
     job.setReducerClass(Reduce.class);
